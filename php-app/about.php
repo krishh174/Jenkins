54bf8a53d9c74fe70e9289508bec7293775a79bc
Sai Krishna <krishh11234@gmail.com>
2018-10-01 01:39:28 +0000

    modified php-app

--- php-app/about.php
+++ php-app/about.php
@@ -11,13 +11,13 @@
 
 <div class="container" id="main-content">
 	<h2>About Us</h2>
-	<p>"About Us" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
+	<p>This is a PHP website running on a docker container with minimal required resources. </p>
 
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	It is developed as a part of Continuous Integration (CI) & Continuous Delivery (CD) of containers using Jenkins.
 	</p>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Developed & deployed by krishh.
 	</p>
 </div>
 
