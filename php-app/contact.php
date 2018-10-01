bbfae27a9865adbda6218556c6c381b393f08388
Sai Krishna <krishh11234@gmail.com>
2018-10-01 01:22:01 +0000

    modified php-app

---
+++ php-app/contact.php
@@ -0,0 +1,27 @@
+<?php include("a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("design-top.php");?>
+<?php include("navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>Contact Us</h2>
+	<p>"Contact Us" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+</div>
+
+<?php include("footer.php");?>
+
+</body>
+</html>
